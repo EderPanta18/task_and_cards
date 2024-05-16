e56f8a589bfadaee21a66bb1dd84d2b9f4023c29
EderPanta18 <ederpanta181005@gmail.com>
2024-05-16 18:20:52 -0500

    saved

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://EderPanta18.github.io/task_and_cards/"
 })
