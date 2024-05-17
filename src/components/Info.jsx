ed1cd7c31928565b1cc4b137b227d5717484c47d
EderPanta18 <ederpanta181005@gmail.com>
2024-05-16 19:02:44 -0500

    saves

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -54,7 +54,7 @@ function Info() {
                         </div>
                     </ul>
                     <span className="absolute bottom-1 " >- Eder Panta</span>
-                    <a href="https://github.com/EderPanta18" target="_blank" className="absolute bottom-1 right-1">
+                    <a href="https://github.com/EderPanta18" target="_blank" className="absolute bottom-0 right-0 w-9 h-9">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 hover:scale-110 duration-200">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
                         </svg>
